73280579aa6ef234452a3c1f1446ef3c7274f1f2
ZineddineBk09 <eino1209@gmail.com>
2021-11-05 10:45:59 +0100

    commited

---
+++ script.js
@@ -0,0 +1,55 @@
+const SIGNINBTN = document.getElementById("signinBtn");
+const USERNAME = document.getElementById("username");
+const PASSWORD = document.getElementById("password");
+const FORM = document.getElementById("form");
+
+const logedInUser = { username: "", password: "" };
+
+//Add users with privelege to access the app
+const privilegeUsers = [
+  { username: "directrice", password: "directrice123" },
+  { username: "scolarite", password: "scolarite123" },
+];
+
+//count the times that user try to sign in
+let countLogins = 0;
+
+//configure the username and password fields
+USERNAME.addEventListener("change", function () {
+  //This input has changed
+  logedInUser.username = USERNAME.value;
+  console.log("username : ", USERNAME.value);
+});
+
+PASSWORD.addEventListener("change", function () {
+  //This input has changed
+  logedInUser.password = PASSWORD.value;
+});
+
+FORM.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+//configure the click event of the Sign In button
+SIGNINBTN.addEventListener("click", () => {
+  if (countLogins < 3) {
+    //check if the directrice or the scolarity loged in
+    if (
+      (logedInUser.username === privilegeUsers[0].username &&
+        logedInUser.password === privilegeUsers[0].password) ||
+      (logedInUser.username === privilegeUsers[1].username &&
+        logedInUser.password === privilegeUsers[1].password)
+    ) {
+      window.location.href = "./index2.html";
+    } else {
+      countLogins += 1;
+      console.log(3 - countLogins, " times left");
+      USERNAME.value = "";
+      USERNAME.style.border = "1px solid red";
+      PASSWORD.value = "";
+      PASSWORD.style.border = "1px solid red";
+    }
+  } else {
+    window.location.href = "./Error.html";
+  }
+});
